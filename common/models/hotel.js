f2fa2b81dffe1c04b4b25a64068014ecb9ff29d0
Alvaro.Soler <6766722@alu.murciaeduca.es>
2018-12-14 02:39:59 +0100

    Fin del examen

--- common/models/hotel.js
+++ common/models/hotel.js
@@ -1,5 +1,21 @@
 'use strict';
 
 module.exports = function(Hotel) {
-
+  Hotel.afterRemote('create', function (context, hotel, next) {
+    var habitaciones = Hotel.app.models.Habitacion;
+    habitaciones.create([{
+        numero: '101',
+        categoria: 'Individual',
+        precio: '60'
+      }, {
+        numero: '102',
+        categoria: 'Doble',
+        precio: '75'
+      }, {
+        numero: '103',
+        categoria: 'Suite',
+        precio: '100'
+      }]);
+    next();
+  });
 };
--- common/models/reserva.js
+++ common/models/reserva.js
@@ -1,5 +1,41 @@
 'use strict';
 
 module.exports = function(Reserva) {
-
+  Reserva.afterRemote('create', function (context, reserva, next) {
+    var desde = context.args.data.desde;
+    var hasta = context.args.data.hasta;
+    if(desde>hasta){
+        /*Suelta internal server error y manda el mensaje al terminal*/
+        throw"La fecha de inicio de la reserva no puede ser menor"
+    }
+    next();
+  });
+  
+  /**
+ * Devuelve las habitacion que no tengan reserva actualmente
+ * @param {Function(Error, array)} callback
+ */
+/* No funciona el 7
+Reserva.primeraLibre = function(callback) {
+  var libres=[];
+  var habitaciones = Reserva.app.models.Habitacion;
+  var hoy = Date.now();
+  habitaciones.forEach(function(habitacion){
+  Reserva.forEach(function(reserva){
+      var desde=reserva.desde;
+      var hasta=reserva.hasta;
+      if(reserva.habitacioId == habitacion.id){
+          if(desde<=hoy && hasta=> hoy){
+          
+      }else{
+          libres+=habitacion;
+      }
+      }
+      
+  });
+  });
+  
+  callback(null, libres);
+};
+*/
 };
